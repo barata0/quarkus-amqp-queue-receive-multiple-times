2bb36011b4756ed420833ba0b1708d1633196b9c
Daniel Ribeiro <danieltribeiro@gmail.com>
2019-06-02 12:17:14 -0300

    refactor: delete AmqpReceiver

--- src/main/java/my/App.java
+++ src/main/java/my/App.java
@@ -1,7 +1,8 @@
 package my;
 
-import java.util.Date;
-import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -9,27 +10,45 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.smallrye.reactive.messaging.annotations.Emitter;
 import io.smallrye.reactive.messaging.annotations.Stream;
 
 @Path("/app")
 public class App {
 
-    // used to generate unique messages
-    private static int helloCount = 0;
-    private static synchronized String nextHello() {
-        return "hello " + helloCount++;
-    }
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    private static AtomicInteger ATOMIC_INTEGER = new AtomicInteger();
 
     @Inject
     @Stream("my-amqp-output")
-    Emitter<String> emitter;
+    private Emitter<String> emitter;
 
+    /**
+     * Emit a new Hello Message to the stream (AMQP queue)!
+     * @return
+     */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() throws InterruptedException {
-        String s = nextHello();
-        emitter.send(s); // send the message to amq with unique payload for debugging
-        return "hello sent: " + s;
+    public String hello() {
+        String s = "Hello " + ATOMIC_INTEGER.incrementAndGet() + "!";
+        emitter.send(s);
+        return s;
     }
+    
+    /**
+     * Process all the messages on the queue
+     * @param input
+     * @return
+     */
+    @Incoming("my-amqp-input")
+    public CompletionStage<Void> receive(String input) {
+        logger.info("received message with payload: " + input);
+        return CompletableFuture.runAsync(new DummyWorker(input));
+    }
+
 }
